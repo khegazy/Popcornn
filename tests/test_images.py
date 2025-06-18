8b03a6f3fdfe4f728f6b296f2c1394bb67b7f0c8
ericyuan00000 <ericyuan@berkeley.edu>
2025-06-17 18:38:55 -0700

    Add pt tests

--- tests/test_images.py
+++ tests/test_images.py
@@ -22,3 +22,23 @@ def test_wolfe_potential():
 
     images = process_images('images/wolfe.npy', device=torch.device('cpu'), dtype=torch.float64)
     assert images.positions.dtype == torch.float64
+
+    images = process_images('images/wolfe.pt', device=torch.device('cpu'), dtype=torch.float32)
+    assert images.image_type is torch.Tensor
+    assert images.positions.shape == (2, 2)
+    assert images.positions.device == torch.device('cpu')
+    assert images.positions.dtype == torch.float32
+    assert torch.allclose(images.positions, torch.tensor([[1.133, -1.486], [-1.166, 1.477]]))
+    assert images.fix_positions.shape == (2,)
+    assert images.fix_positions.device == torch.device('cpu')
+    assert images.fix_positions.dtype == torch.bool
+    assert torch.allclose(images.fix_positions, torch.zeros(2, dtype=torch.bool))
+    assert images.atomic_numbers is None
+    assert images.pbc is None
+    assert images.cell is None
+    assert images.tags is None
+    assert images.charge is None
+    assert images.spin is None
+
+    images = process_images('images/wolfe.pt', device=torch.device('cpu'), dtype=torch.float64)
+    assert images.positions.dtype == torch.float64
