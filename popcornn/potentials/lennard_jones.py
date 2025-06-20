f81c56fae3c4779320fe1cc161e32fb44645049b
ericyuan00000 <ericyuan@berkeley.edu>
2025-06-19 19:59:09 -0700

    Add lj tests

--- popcornn/potentials/lennard_jones.py
+++ popcornn/potentials/lennard_jones.py
@@ -4,26 +4,29 @@ from ase.data import covalent_radii
 from .base_potential import BasePotential, PotentialOutput
 
 class LennardJones(BasePotential):
-    def __init__(self, **kwargs):
+    def __init__(self, epsilon=1.0, sigma=1.0, **kwargs):
         """
         Constructor for the Lennard-Jones Potential.
 
         The potential is given by:
-        E_ij = (r0_ij / r_ij)^12 - 2 * (r0_ij / r_ij)^6
+        E_ij = 4 * epsilon * ((sigma / r_ij) ** 12 - (sigma / r_ij) ** 6)
         E = sum_{i<j} E_ij
 
-        Parameters
-        ----------
+        r_ij is the distance between atoms i and j, under minimum image convention.
         """
         super().__init__(**kwargs)
-        self.r0 = None
+        assert self.n_atoms is not None, "Number of atoms must be defined."
+        self.ind = torch.triu_indices(self.n_atoms, self.n_atoms, offset=1, device=self.device)
+        self.epsilon = epsilon
+        self.sigma = sigma
     
     def forward(self, positions):
-        if self.r0 is None:
-            self.set_r0(self.atomic_numbers)
         positions_3d = positions.view(-1, self.n_atoms, 3)
-        r = torch.norm(positions_3d[:, self.ind[0]] - positions_3d[:, self.ind[1]], dim=-1)
-        energies_decomposed = (self.r0 / r) ** 12 - 2 * (self.r0 / r) ** 6
+        r = positions_3d[:, self.ind[0]] - positions_3d[:, self.ind[1]]
+        if self.pbc.any():
+            r = wrap_positions(r, self.cell, self.pbc, center=1.0)
+        r = torch.norm(r, dim=-1)
+        energies_decomposed = 4 * self.epsilon * ((self.sigma / r) ** 12 - (self.sigma / r) ** 6)
         energies = torch.sum(energies_decomposed, dim=-1, keepdim=True)
 
         forces = self.calculate_conservative_forces(energies, positions)
@@ -34,13 +37,3 @@ class LennardJones(BasePotential):
             forces=forces,
             forces_decomposed=forces_decomposed
         )
-    
-    def set_r0(self, atomic_numbers):
-        """
-        Set the r0_ij values for the potential
-        """
-        radii = torch.tensor([covalent_radii[n] for n in atomic_numbers], device=self.device)
-        r0 = radii.view(-1, 1) + radii.view(1, -1)
-        self.ind = torch.triu_indices(r0.shape[0], r0.shape[1], offset=1, device=self.device)
-        self.r0 = r0[None, self.ind[0], self.ind[1]]
-
--- tests/test_potentials.py
+++ tests/test_potentials.py
@@ -13,8 +13,7 @@ from popcornn.potentials import get_potential
 def test_wolfe(dtype):
     images = process_images('images/wolfe.json', device=torch.device('cpu'), dtype=dtype)
     path = get_path('linear', images=images, unwrap_positions=False, device=torch.device('cpu'), dtype=dtype)
-    potential = get_potential(images=images, name='wolfe_schlegel', device=torch.device('cpu'), dtype=dtype)
-    assert potential is not None
+    potential = get_potential('wolfe_schlegel',images=images, device=torch.device('cpu'), dtype=dtype)
 
     potential_output = potential(path(torch.tensor([0.0, 0.5, 1.0], requires_grad=True, device=torch.device('cpu'), dtype=dtype)).positions)
     assert potential_output.energies.shape == (3, 1)
@@ -33,6 +32,82 @@ def test_wolfe(dtype):
                 [0.0032145200000005536, 0.04517024000000619], 
                 [-2.614820315, -1.194996355], 
                 [-0.0003081600000115481, -0.06473332000001358]
+            ], 
+            dtype=dtype
+        ),
+        atol=1e-3
+    )
+    assert potential_output.forces.grad_fn is not None
+
+
+@pytest.mark.parametrize(
+    'dtype',
+    [torch.float32, torch.float64],
+)
+def test_lennard_jones(dtype):
+    images = process_images('images/LJ13.xyz', device=torch.device('cpu'), dtype=dtype)
+    path = get_path('linear', images=images, unwrap_positions=False, device=torch.device('cpu'), dtype=dtype)
+    potential = get_potential('lennard_jones', images=images, device=torch.device('cpu'), dtype=dtype)
+
+    potential_output = potential(path(torch.tensor([0.0, 0.5, 1.0], requires_grad=True, device=torch.device('cpu'), dtype=dtype)).positions)
+    assert potential_output.energies.shape == (3, 1)
+    assert potential_output.energies.device == torch.device('cpu')
+    assert potential_output.energies.dtype == dtype
+    assert torch.allclose(potential_output.energies,
+        torch.tensor([[-44.326801419534], [-34.906096032753304], [-44.326801419534]], dtype=dtype),
+        atol=1e-5
+    )
+    assert potential_output.forces.shape == (3, 39)
+    assert potential_output.forces.device == torch.device('cpu')
+    assert potential_output.forces.dtype == dtype
+    assert torch.allclose(potential_output.forces,
+        torch.tensor(
+            [
+                [
+                    -1.22026872e-07,  4.65947863e-07, -8.69690254e-07,
+                     1.46976010e-07, -1.02301429e-06, -3.09678949e-07,
+                    -2.95339901e-07,  3.82525294e-07,  3.71750888e-07,
+                     7.84620041e-09,  2.96609302e-07,  5.88354190e-07,
+                    -2.14042780e-07,  2.89353779e-07,  1.63606897e-07,
+                     2.95339901e-07, -3.82525294e-07, -3.71750888e-07,
+                     2.14042780e-07, -2.89353779e-07, -1.63606897e-07,
+                     4.38490273e-07, -7.83095954e-07,  1.05010643e-07,
+                    -4.38490273e-07,  7.83095954e-07, -1.05010643e-07,
+                    -7.84620058e-09, -2.96609302e-07, -5.88354190e-07,
+                     1.22026872e-07, -4.65947863e-07,  8.69690255e-07,
+                    -1.46976010e-07,  1.02301429e-06,  3.09678949e-07,
+                     0.00000000e+00,  4.44089210e-16, -1.29930788e-15
+                ],
+                [
+                     4.48349762e+00, -1.32186255e+01,  1.41587260e+01,
+                    -7.82475022e+00, -1.89677327e+01,  1.86819082e+00,
+                     6.72522859e+00, -1.35441595e+01, -1.48608792e+01,
+                    -1.24385188e+01, -5.71574566e+00,  1.38198016e+01,
+                     1.43742609e+01,  5.38388330e+00,  1.51975831e+01,
+                    -6.72522859e+00,  1.35441595e+01,  1.48608792e+01,
+                    -1.43742609e+01, -5.38388330e+00, -1.51975831e+01,
+                     1.89497235e+01, -7.81237880e+00, -3.58365115e-01,
+                    -1.89497235e+01,  7.81237880e+00,  3.58365115e-01,
+                     1.24385188e+01,  5.71574566e+00, -1.38198016e+01,
+                    -4.48349762e+00,  1.32186255e+01, -1.41587260e+01,
+                     7.82475022e+00,  1.89677327e+01, -1.86819082e+00,
+                     4.44089210e-16,  8.88178420e-16,  2.77555756e-16
+                ],
+                [
+                     2.89353779e-07,  2.14042780e-07,  1.63606897e-07,
+                    -7.83095954e-07, -4.38490272e-07,  1.05010643e-07,
+                    -2.96609302e-07,  7.84620080e-09, -5.88354190e-07,
+                     4.65947863e-07,  1.22026872e-07, -8.69690254e-07,
+                    -3.82525294e-07, -2.95339901e-07, -3.71750888e-07,
+                     2.96609302e-07, -7.84620058e-09,  5.88354190e-07,
+                     3.82525294e-07,  2.95339901e-07,  3.71750888e-07,
+                     1.02301429e-06,  1.46976010e-07,  3.09678949e-07,
+                    -1.02301429e-06, -1.46976010e-07, -3.09678949e-07,
+                    -4.65947863e-07, -1.22026872e-07,  8.69690255e-07,
+                    -2.89353779e-07, -2.14042780e-07, -1.63606896e-07,
+                     7.83095954e-07,  4.38490272e-07, -1.05010643e-07,
+                     0.00000000e+00,  8.88178420e-16,  1.87350135e-16
+                ]
             ], 
             dtype=dtype
         ),
