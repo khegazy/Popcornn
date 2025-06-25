1773fcf6bbfc23cb7070b422c4213759f53bcddd
ericyuan00000 <ericyuan@berkeley.edu>
2025-06-25 16:21:47 -0700

    Remove minimum image convention and add neighbor list

--- popcornn/potentials/lennard_jones.py
+++ popcornn/potentials/lennard_jones.py
@@ -2,9 +2,10 @@ import torch
 from ase.data import covalent_radii
 
 from .base_potential import BasePotential, PotentialOutput
+from popcornn.tools import radius_graph
 
 class LennardJones(BasePotential):
-    def __init__(self, epsilon=1.0, sigma=1.0, **kwargs):
+    def __init__(self, epsilon=1.0, sigma=1.0, cutoff=3.0, **kwargs):
         """
         Constructor for the Lennard-Jones Potential.
 
@@ -16,24 +17,44 @@ class LennardJones(BasePotential):
         """
         super().__init__(**kwargs)
         assert self.n_atoms is not None, "Number of atoms must be defined."
-        self.ind = torch.triu_indices(self.n_atoms, self.n_atoms, offset=1, device=self.device)
         self.epsilon = epsilon
         self.sigma = sigma
+        self.cutoff = cutoff
     
     def forward(self, positions):
         positions_3d = positions.view(-1, self.n_atoms, 3)
-        r = positions_3d[:, self.ind[0]] - positions_3d[:, self.ind[1]]
-        if self.pbc.any():
-            r = wrap_positions(r, self.cell, self.pbc, center=1.0)
-        r = torch.norm(r, dim=-1)
-        energies_decomposed = 4 * self.epsilon * ((self.sigma / r) ** 12 - (self.sigma / r) ** 6)
-        energies = torch.sum(energies_decomposed, dim=-1, keepdim=True)
+        graph_dict = radius_graph(
+            positions=positions_3d,
+            cell=self.cell,
+            pbc=self.pbc,
+            cutoff=self.cutoff,
+            max_neighbors=-1,
+        )
+        r = graph_dict['edge_distance'].to(dtype=self.dtype)
+        v = graph_dict['edge_distance_vec'].to(dtype=self.dtype)
+        # energies_decomposed = 4 * self.epsilon * ((self.sigma / r) ** 12 - (self.sigma / r) ** 6)
+        energies_part = (
+            4 * self.epsilon * ((self.sigma / r) ** 12 - (self.sigma / r) ** 6) 
+            - 4 * self.epsilon * (
+                (self.sigma / self.cutoff) ** 12 - (self.sigma / self.cutoff) ** 6
+            )
+        ).unsqueeze(-1)
+        # energies = torch.sum(energies_decomposed, dim=-1, keepdim=True)
+        energies = torch.zeros((len(positions_3d), 1), device=self.device, dtype=self.dtype)
+        energies.index_add_(0, graph_dict['edge_index'][1] // self.n_atoms, energies_part)
+        energies = energies / 2
 
-        forces = self.calculate_conservative_forces(energies, positions)
-        forces_decomposed = self.calculate_conservative_forces_decomposed(energies_decomposed, positions)
+        forces_part = (
+            -24 * self.epsilon * (2 * (self.sigma / r) ** 12 - (self.sigma / r) ** 6) / r ** 2
+        ).unsqueeze(-1) * v
+        forces = torch.zeros((len(positions_3d) * self.n_atoms, 3), device=self.device, dtype=self.dtype)
+        forces.index_add_(0, graph_dict['edge_index'][1], forces_part)
+        forces = forces.view(len(positions_3d), self.n_atoms * 3)
+        # forces = self.calculate_conservative_forces(energies, positions)
+        # forces_decomposed = self.calculate_conservative_forces_decomposed(energies_decomposed, positions)
         return PotentialOutput(
             energies=energies,
-            energies_decomposed=energies_decomposed,
+            # energies_decomposed=energies_decomposed,
             forces=forces,
-            forces_decomposed=forces_decomposed
+            # forces_decomposed=forces_decomposed
         )
